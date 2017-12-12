0eb9ed118d651b7a4f0a482a00025280bf146e90
William Spangler <bspangs@hotmail.com>
2017-12-12 18:56:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,7 @@ var chocolateBars = ["snickers", "hundred grand","kitkat","skittles"];
 //does not alter original array,returns a new one
 
 function addElementToBeginningOfArray (array,element){
-//var array = `${array}`;  // do I need quotes
+var array = `${array}`;  // do I need quotes
 var moreArray=`${array}`.push(`${element}`);
 return moreArray;
 }
